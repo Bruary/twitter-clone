01cdd6aae360340ba2c67a532a27af81e0a795dd
Abubakir Kais <bakir_janabi@yahoo.com>
2021-08-18 01:19:49 +0400

    Adding age validation

--- validate/validate.go
+++ validate/validate.go
@@ -10,6 +10,10 @@ func IsNumberNegative(number int) bool {
 	return number < 0
 }
 
+func IsAge12AndAbove(number int) bool {
+	return number >= 12
+}
+
 func IsPasswordLengthCorrect(password string) bool {
 	return len(password) >= PasswordMinLength
 }
