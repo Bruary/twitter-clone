c8e452fef90e7db9f7ecc42f8337ca0b60a0ec3e
Abubakir Kais <bakir_janabi@yahoo.com>
2021-08-26 17:58:47 +0400

    Adjusted some function to use UUID, added getTweets function

--- db/db.go
+++ db/db.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	models "github.com/Bruary/twitter-clone/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -55,8 +56,8 @@ func UserAlreadyExists(dbCollection *mongo.Collection, usersEmail string) (bool,
 	}
 }
 
-func GetDocumentFromDB(dbCollection *mongo.Collection, userEmail string) (*mongo.SingleResult, error) {
-	result := dbCollection.FindOne(context.TODO(), bson.M{"email": userEmail})
+func GetDocFromDBUsingEmail(dbCollection *mongo.Collection, usersEmail string) (*mongo.SingleResult, error) {
+	result := dbCollection.FindOne(context.TODO(), bson.M{"email": usersEmail})
 	if result.Err() == mongo.ErrNoDocuments {
 		return result, result.Err()
 	}
@@ -65,11 +66,67 @@ func GetDocumentFromDB(dbCollection *mongo.Collection, userEmail string) (*mongo
 
 }
 
-func DeleteUser(dbCollection *mongo.Collection, usersEmail string) error {
-	_, err := dbCollection.DeleteOne(context.TODO(), bson.M{"email": usersEmail})
+func GetDocFromDBUsingUUID(dbCollection *mongo.Collection, usersUUID string) (*mongo.SingleResult, error) {
+	result := dbCollection.FindOne(context.TODO(), bson.M{"uuid": usersUUID})
+	if result.Err() == mongo.ErrNoDocuments {
+		return result, result.Err()
+	}
+
+	return result, nil
+
+}
+
+func DeleteUser(dbCollection *mongo.Collection, usersUUID string) error {
+	_, err := dbCollection.DeleteOne(context.TODO(), bson.M{"uuid": usersUUID})
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func GetAllMatchingDocuments(dbCollection *mongo.Collection, userUUID string) ([]bson.M, error) {
+	cursor, err := dbCollection.Find(context.TODO(), bson.M{"useruuid": userUUID})
+	if err != nil {
+		return nil, err
+	}
+
+	var result []bson.M
+
+	err2 := cursor.All(context.TODO(), &result)
+	if err2 != nil {
+		return nil, err2
+	}
+
+	return result, nil
+}
+
+func GetTweets(dbCollection *mongo.Collection, userUUID string) ([]models.Tweet, error) {
+	var tweets []models.Tweet
+
+	// ignore the following fields and return the rest from the db
+	var projection = bson.M{
+		"useruuid":  0,
+		"email":     0,
+		"createdat": 0,
+		"updatedat": 0,
+	}
+
+	cursor, err := dbCollection.Find(context.TODO(), bson.M{"useruuid": userUUID}, options.Find().SetProjection(projection))
+	if err != nil {
+		return tweets, err
+	}
+
+	// If there are no tweets then return an empty string
+	if cursor.RemainingBatchLength() == 0 {
+		return []models.Tweet{}, nil
+	}
+
+	// Decode all the tweets from the db to the Tweets struct
+	err2 := cursor.All(context.TODO(), &tweets)
+	if err2 != nil {
+		return tweets, err2
+	}
+
+	return tweets, nil
 }
