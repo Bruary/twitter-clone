fb40253e7fb71bca3ba497c052cd5e9af496b232
Abubakir Kais <bakir_janabi@yahoo.com>
2021-09-05 23:06:14 +0400

    adjusting the interface, changing the request and response for each endpoint

--- service/service.go
+++ service/service.go
@@ -1,11 +1,14 @@
 package service
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Bruary/twitter-clone/service/models"
+	"github.com/gofiber/fiber/v2"
+)
 
 type Service interface {
-	CreateUser(*fiber.Ctx) error
+	CreateUser(*fiber.Ctx, models.CreateUserRequest) models.BaseResponse
+	SignIn(*fiber.Ctx, models.SignInRequest) *models.SignInResponse
 	DeleteUser(*fiber.Ctx) error
-	SignIn(*fiber.Ctx) error
 	CreateTweet(*fiber.Ctx) error
 	GetTweets(*fiber.Ctx) error
 	Follow(*fiber.Ctx) error
