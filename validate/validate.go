4508561d41bdc58d6d0af6d683c334f4d7a597ec
Abubakir Kais <bakir_janabi@yahoo.com>
2021-09-07 23:17:09 +0400

    added token validation functions to validate

--- validate/validate.go
+++ validate/validate.go
@@ -1,6 +1,13 @@
 package validate
 
+import (
+	"github.com/Bruary/twitter-clone/service/models"
+	"github.com/dgrijalva/jwt-go"
+)
+
 const PasswordMinLength = 8
+
+var jwtKey = []byte("White_Yasmin")
 
 func IsStringEmpty(text string) bool {
 	return text == ""
@@ -16,4 +23,22 @@ func IsAge12AndAbove(number int) bool {
 
 func IsPasswordLengthCorrect(password string) bool {
 	return len(password) >= PasswordMinLength
+}
+
+func IsTokenValid(tokenString string) bool {
+	claims := &models.Claims{}
+	token, _ := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	return token.Valid
+}
+
+func GetJWTclaims(tokenString string) *models.Claims {
+
+	claims := &models.Claims{}
+	jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	return claims
 }
