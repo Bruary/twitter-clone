b97c348db0d5f1dd4863b65e4593810756b33333
Abubakir Kais <bakir_janabi@yahoo.com>
2021-09-10 02:36:08 +0400

    Added a separate function to generate JWT

---
+++ service/twitter/create_jwt.go
@@ -0,0 +1,31 @@
+package twitter
+
+import (
+	"time"
+
+	"github.com/Bruary/twitter-clone/service/models"
+	"github.com/dgrijalva/jwt-go"
+)
+
+func CreateJWT(userUUID string, accountID string, validDurationMinutes time.Duration) (string, error) {
+
+	// create the claims that will be used in the JWT token
+	claims := &models.Claims{
+		User_UUID:  userUUID,
+		Account_ID: accountID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(validDurationMinutes * time.Minute).Unix(),
+		},
+	}
+
+	// declaring the token with the method used for signing along with the claims§
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Create the JWT string
+	tokenString, err4 := token.SignedString(jwtKey)
+	if err4 != nil {
+		return tokenString, err4
+	}
+
+	return tokenString, nil
+}
